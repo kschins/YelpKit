fe4d13d9ac9468208992fedb501935500a294826
Kasey Schindler <me@kaseyschindler.com>
2018-10-26 11:23:31 -0600

    Last file that needs to be integrated into the API client for things to work via Cocoapods. The code works as a project within a project...need to figure out what the deal is...

--- YelpKit/YelpKit/YelpAPIClient.swift
+++ YelpKit/YelpKit/YelpAPIClient.swift
@@ -86,4 +86,40 @@ public final class YelpAPIClient {
             return parameters.map { URLQueryItem(name: $0, value: $1.description) }
         }
     }
+    
+    private enum HTTPParameter: CustomStringConvertible, Decodable {
+        case string(String)
+        case bool(Bool)
+        case int(Int)
+        case double(Double)
+        
+        init(from decoder: Decoder) throws {
+            let container = try decoder.singleValueContainer()
+            
+            if let string = try? container.decode(String.self) {
+                self = .string(string)
+            } else if let bool = try? container.decode(Bool.self) {
+                self = .bool(bool)
+            } else if let int = try? container.decode(Int.self) {
+                self = .int(int)
+            } else if let double = try? container.decode(Double.self) {
+                self = .double(double)
+            } else {
+                throw YelpError.decoding
+            }
+        }
+        
+        var description: String {
+            switch self {
+            case .string(let string):
+                return string
+            case .bool(let bool):
+                return String(describing: bool)
+            case .int(let int):
+                return String(describing: int)
+            case .double(let double):
+                return String(describing: double)
+            }
+        }
+    }
 }
