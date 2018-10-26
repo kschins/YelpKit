3353f67dbc0440e902974d51be0c2031a0edb657
Kasey Schindler <me@kaseyschindler.com>
2018-10-26 11:14:53 -0600

    Change URLQueryItemEncoder to struct from an enum.

--- YelpKit/YelpKit/Utilities/URLQueryItemEncoder.swift
+++ YelpKit/YelpKit/Utilities/URLQueryItemEncoder.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 /// Encodes any encodable to a URLQueryItem list
-public enum URLQueryItemEncoder {
+struct URLQueryItemEncoder {
     static func encode<T: Encodable>(_ encodable: T) throws -> [URLQueryItem] {
         let encoder = JSONEncoder()
         encoder.keyEncodingStrategy = .convertToSnakeCase
